eb8b5724b406d35762e23389d3b6bf9ac4b02571
krishna sahu <krishna.sahu.work@gmail.com>
2026-02-02 22:55:21 +0530

    Ai function inngest added

--- Backend/src/lib/ENV.ts
+++ Backend/src/lib/ENV.ts
@@ -5,6 +5,7 @@ export const ENV = {
 PORT:process.env.PORT,
 MONGO_URL:process.env.MONGO_URL,
 JWT_USER_SECRET:process.env.JWT_USER_SECRET,
-NODE_ENV:process.env.NODE_ENV
+NODE_ENV:process.env.NODE_ENV,
+OPEN_API_KEY:process.env.OPEN_API_KEY
 
 }
