9810ff5e99470cc1ae6abbaf8b41a76a0caec75a
krishna sahu <krishna.sahu.work@gmail.com>
2026-02-09 23:23:45 +0530

    Signup function done

---
+++ frontend/lib/auth/signup.ts
@@ -0,0 +1,31 @@
+import { ENV } from "../env";
+import axios from "axios";
+
+interface Data {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export async function signupUser(data: Data) {
+  try {
+    const res = await axios.post(
+      ENV.BACKEND_SIGNUP_URL as string,
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return res.data;
+  } catch (error: any) {
+    const message =
+      error?.response?.data?.message ||
+      error.message ||
+      "Signup failed";
+
+    throw new Error(message);
+  }
+}
