c182f2b5fc9edc3fca167bd54080fc85fb2f758b
krishna sahu <krishna.sahu.work@gmail.com>
2026-02-14 00:56:16 +0530

    User details update function created

---
+++ frontend/lib/update/updateuserdetails.ts
@@ -0,0 +1,39 @@
+import axios from "axios"
+import { ENV } from "../env"
+
+interface UserDetails {
+ 
+    currentpassword:string,
+    newemail?:string,
+    newusername?:string,
+    newpassword?:string
+
+}
+
+interface UpdateResponse {
+  success: boolean
+  message: string
+  user?: {
+    id: string
+    email: string
+    username: string
+  }
+}
+export async function updateUserDetails(data:UserDetails):Promise<UpdateResponse>{
+
+try{
+
+const res = await axios.post(ENV.BACKEND_UPDATE_URL as string ,data,{withCredentials:true})
+
+return res.data
+}catch(err : any){
+ const message =
+            err?.response?.data?.message ||
+            err.message ||
+           "Failed to update user details";
+
+        throw new Error(message);
+}
+
+
+}
