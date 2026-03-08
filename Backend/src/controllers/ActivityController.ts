6be38f66132d68a945cd46aadee64368fbb8dda8
krishna sahu <krishna.sahu.work@gmail.com>
2026-03-09 00:08:07 +0530

    Backend get user activites code added

--- Backend/src/controllers/ActivityController.ts
+++ Backend/src/controllers/ActivityController.ts
@@ -47,3 +47,34 @@ export const logActivity = async (req: Request, res: Response, next: NextFunctio
     }
 
 }
+
+export const getUserActivities = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const userId = req.user._id;
+
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: "User not authenticated",
+      });
+    }
+
+    const activities = await Activity.find({ userId }).sort({
+      timestamp: -1,
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: activities,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
--- Backend/src/routes/ActivityRoute.ts
+++ Backend/src/routes/ActivityRoute.ts
@@ -1,12 +1,13 @@
 import express  from "express"
 import { AuthMiddleware } from "../middlewares/AuthMiddleware.js"
-import { logActivity } from "../controllers/ActivityController.js"
+import { getUserActivities, logActivity } from "../controllers/ActivityController.js"
 
 const ActivityRouter = express.Router()
 
 ActivityRouter.use(AuthMiddleware)
 
 
+ActivityRouter.get("/useractivity", getUserActivities);
 ActivityRouter.post("/useractivity",logActivity)
 
 export default ActivityRouter
