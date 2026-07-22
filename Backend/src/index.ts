85ab0fb07376fabcc71cceeecd89ccce4a624587
krishna sahu <krishna.sahu.work@gmail.com>
2026-07-22 23:33:52 +0530

    health check route add

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -10,6 +10,7 @@ import ActivityRouter from "./routes/ActivityRoute.js";
 import MoodRouter from "./routes/MoodRoute.js";
 import ChatRouter from "./routes/ChatRoute.js";
 import helmet from "helmet";
+import HealthRouter from "./routes/HealthRoute.js";
 
 const app = express()
 app.use(cors({origin:'http://localhost:3000',credentials:true}))
@@ -18,9 +19,10 @@ app.use(express.json())
 app.use(cookieParser())
 app.use("/api/inngest", serve({ client: inngest, functions: inngestFunctions }));
 
-app.use('/api/user', UserRouter)
-app.use('/api/activity',ActivityRouter)
-app.use('/api/mood',MoodRouter)
-app.use('/api/chat',ChatRouter)
+app.use('/api/user', UserRouter);
+app.use('/api/activity',ActivityRouter);
+app.use('/api/mood',MoodRouter);
+app.use('/api/chat',ChatRouter);
+app.use('/api/v1/health',HealthRouter);
 
-startServer(app)
+startServer(app);
